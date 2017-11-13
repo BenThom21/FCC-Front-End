972f13a50a2fbcc185aeb8d50ec878e0a7515aa5
Ben Thompson <benthompson4496@gmail.com>
2017-11-12 20:06:47 -0600

    adding first Gulp task

--- localWeather/js/gulpfile.js
+++ localWeather/js/gulpfile.js
@@ -3,4 +3,9 @@ var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
+  gulp.task('sass', function(){
+    return gulp.src('source-files')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('CSS/styles.css'))
+  });
 });
