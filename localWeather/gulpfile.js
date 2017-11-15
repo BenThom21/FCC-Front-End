b68ac9a1ad180e33171cd9926d4ada8260f1d67f
Ben Thompson <benthompson4496@gmail.com>
2017-11-14 21:48:11 -0600

    gulp and watch are working!

--- localWeather/gulpfile.js
+++ localWeather/gulpfile.js
@@ -2,13 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function() {
-  // place code for your default task here
+  // default task code
   console.log('Hello world');
 });
 
-// Change destinations below
+// Compile Sass files to CSS
 gulp.task('sass', function(){
-  return gulp.src('site/CSS/style.scss')
+  return gulp.src('site/CSS/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('site/CSS'))
 });
+
+//Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('site/CSS/*.scss', ['sass']); //['sass'] is the task to be run when changes happen
+})
