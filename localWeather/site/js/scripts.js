ac8ec3f3c17eb69386d79b85426b6d6c6918ea8e
Ben Thompson <benthompson4496@gmail.com>
2017-11-20 18:30:13 -0600

    geolocation in js

--- localWeather/site/js/scripts.js
+++ localWeather/site/js/scripts.js
@@ -1,2 +1,28 @@
 // No "script" tags in this file!
 
+//HTML5 Geolocation
+function geoFindMe() {
+    var output = document.getElementById("loc");
+
+    if (!navigator.geolocation){
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
+
+    function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        //This will need to change to update exact position
+        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+        
+    }
+
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+
+    output.innerHTML = "<p>Locating…</p>";
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
