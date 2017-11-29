3640924b0b15ea05ab7bd793d5db9e7be5939b16
Ben Thompson <benthompson4496@gmail.com>
2017-11-28 19:00:49 -0600

    switching to Bootstrap and update Gulp file

--- localWeather/gulpfile.js
+++ localWeather/gulpfile.js
@@ -9,11 +9,12 @@ gulp.task('default', function() {
 // Compile Sass files to CSS
 gulp.task('sass', function(){
   return gulp.src('site/CSS/*.scss')
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass()) 
     .pipe(gulp.dest('site/CSS'))
 });
 
 //Watch files for changes
 gulp.task('watch', function() {
-  gulp.watch('site/CSS/*.scss', ['sass']); //['sass'] is the task to be run when changes happen
+  gulp.watch('site/CSS/*.scss', ['sass']); 
+  //['sass'] is the task to be run when changes happen
 })
