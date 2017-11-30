8821a7ce1fe8bc4f2647fee9a8f5ec35ddeda11d
Ben Thompson <benthompson4496@gmail.com>
2017-11-29 21:59:44 -0600

    figuring out geolocation

--- localWeather/site/js/scripts.js
+++ localWeather/site/js/scripts.js
@@ -1,17 +1,29 @@
 // No "script" tags in this file!
 
+var x = document.getElementById('loc');
+function getUserInfo () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
 //HTML5 Geolocation
-$(document).ready(
+$(document).ready(function () {
     var x = document.getElementById("loc");
     function getLocation() {
         if (navigator.geolocation) {
+            console.log('yup');
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
+            console.log('nope');
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
     function showPosition(position) {
+        console.log('showing position');
         x.innerHTML = "Latitude: " + position.coords.latitude + 
         "<br>Longitude: " + position.coords.longitude; 
     }
-);
+});
