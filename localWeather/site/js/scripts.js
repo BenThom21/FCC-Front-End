632f6818ba69b74080c3857107667c64300e6ef2
Ben Thompson <benthompson4496@gmail.com>
2017-12-03 20:39:36 -0600

    got ittttt!

--- localWeather/site/js/scripts.js
+++ localWeather/site/js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             console.log('working');
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
+            location.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
 
@@ -26,9 +26,12 @@ $(document).ready(function () {
         //reverse geocoding using Google API
         var googleGeo = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlong + "&key=AIzaSyC64n2_xlIpSInEFKyx3SMLFA4GhmvnSf0";
         console.log(googleGeo);
+        //FIGURE THIS OUT
         $.getJSON(googleGeo, function(data){
             console.log(data);
-            $('#city').innerHTML = data.results[0].address_components[2];
+            var hood = data.results[0].address_components[2].short_name;
+            console.log(hood);
+            // $('#city').innerHTML = data.results[0].address_components[2];
         });
 
         //change inner HTML to results[0].locality, results[0].administrative_area_level_1 <br> results[0].country
@@ -36,25 +39,6 @@ $(document).ready(function () {
         "<br>Longitude: " + long;
     }
 
-
-    // geocodeLatLng();
-    // function geocodeLatLng(geocoder) {
-    //     var geocoder = new google.maps.Geocoder;
-    //     var input = document.getElementById('latlng').value;
-    //     var latlngStr = input.split(',', 2);
-    //     var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    //     geocoder.geocode({'location': latlng}, function(results, status) {
-    //       if (status === 'OK') {
-    //         if (results[0]) {
-    //           document.getElementById('location').innerHTML(results[0].formatted_address);
-    //         } else {
-    //           alert('No results found');
-    //         }
-    //       } else {
-    //         alert('Geocoder failed due to: ' + status);
-    //       }
-    //     });
-    // }
 
 
 });
