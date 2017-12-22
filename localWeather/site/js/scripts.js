9ca006ad3417cf265c99ebcf30daf2192892be24
Ben Thompson <benthompson4496@gmail.com>
2017-12-22 11:08:37 -0600

    got it working! Thank you StackOverflow

--- localWeather/site/js/scripts.js
+++ localWeather/site/js/scripts.js
@@ -91,18 +91,14 @@ $(document).ready(function() {
 function day() {
     var day = new Date();
     var week = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-    console.log(week);
 
-    for (i=0; i<week.length; i++) {
-
-        console.log(week[day.getDay(i)+1]);
-
-        // var tom = document.getElementById("tomorrow");
-        // tom.innerHTML = week[day.getDay()+1];
-        // var next = document.getElementById("twodays");
-        // next.innerHTML = week[day.getDay()+2];
-        // var two = document.getElementById("threedays");
-        // two.innerHTML = week[day.getDay()+3]; 
+    for (i=0; i<14; i++) {
+        var tom = document.getElementById("tomorrow");
+        tom.innerHTML = week[(day.getDay()+1) % 7];
+        var next = document.getElementById("twodays");
+        next.innerHTML = week[(day.getDay()+2) % 7];
+        var two = document.getElementById("threedays");
+        two.innerHTML = week[(day.getDay()+3) % 7]; 
     }
 
     //OLD WAY THAT GIVES 'UNDEFINED' BY SUNDAY
