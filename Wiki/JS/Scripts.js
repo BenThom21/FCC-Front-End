09da2ed13647bdedf751433ec3874f45c18ead59
Ben Thompson <benthompson4496@gmail.com>
2018-03-11 17:33:26 -0500

    Preparing Wikipedia API

--- Wiki/JS/Scripts.js
+++ Wiki/JS/Scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+        }
+        error: function (errorMessage) {
+        }
+
+    })
 })
