a663e4f0821048971ead9852ab6f0012c976f80c
Ben Thompson <benthompson4496@gmail.com>
2018-03-11 17:54:38 -0500

    update wiki api action and url

--- Wiki/JS/Scripts.js
+++ Wiki/JS/Scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
+    var searchTerm = $('#search').val();
+    var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?",'
     $.ajax({
         type: 'GET',
         dataType: 'json',
         async: false,
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        //Need to change this URL once I figure out how to cut in the search
+        url: url;
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log(data);
-        }
+        },
         error: function (errorMessage) {
         }
 
-    })
+    });
 })
