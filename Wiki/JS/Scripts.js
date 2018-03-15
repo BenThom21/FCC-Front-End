5fb0a3f9911d4261817879c9d61b4f784bf518e5
Ben Thompson <benthompson4496@gmail.com>
2018-03-14 20:38:31 -0500

    Well the error message works, so that's good!

--- Wiki/JS/Scripts.js
+++ Wiki/JS/Scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var searchTerm = $('#searchTerm').val();
-    var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?",'
+    var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
     $.ajax({
         type: 'GET',
         dataType: 'json',
         async: false,
         //Need to change this URL once I figure out how to cut in the search
-        url: url;
+        url: url,
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log(data);
@@ -15,4 +15,4 @@ $(document).ready(function() {
         }
 
     });
-})
+});
